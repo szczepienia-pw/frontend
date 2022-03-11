3c61307fad27c20b20ab9eacfcdc2e8070d28b3a
Brotholomew <bartoszek.blach@gmail.com>
2022-03-11 16:41:40 +0100

    tests working
    Signed-off-by: Brotholomew <bartoszek.blach@gmail.com>

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,1 +1,7 @@
+import { mount } from '@vue/test-utils'
+import LoginPage from '@/views/LoginPage'
 
+test('lorem-ipsum on main scr', () => {
+    const wrapper = mount(LoginPage)
+    expect(wrapper.text()).toContain('Lorem ipsum')
+})
