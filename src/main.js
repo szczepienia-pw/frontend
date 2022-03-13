32f180cbf9356073e7460a3f31a97464a5f082f6
Dawid Alimowski <dawid.alimowski@gmail.com>
2022-03-13 13:19:00 +0100

    [SP-96] Adding vaccination slots by doctors
    Working logic + inputs
    Cleaned up api
    
    Might be moved to a separate view later.

--- src/main.js
+++ src/main.js
@@ -1,7 +1,14 @@
 import { createApp } from 'vue'
 import router from './router/index'
 import PrimeVue from 'primevue/config';
+
+import ToastService from 'primevue/toastservice';
+import Tooltip from 'primevue/tooltip';
 import Button from 'primevue/button';
+import Toast from 'primevue/toast';
+import Calendar from 'primevue/calendar';
+import Card from 'primevue/card';
+
 import App from './App.vue'
 
 import 'primevue/resources/themes/saga-blue/theme.css';
@@ -11,5 +18,10 @@ import 'primeicons/primeicons.css';
 createApp(App)
     .use(router)
     .use(PrimeVue, { ripple: true })
+    .use(ToastService)
+    .directive('tooltip', Tooltip)
     .component('Button', Button)
+    .component('Toast', Toast)
+    .component('Calendar', Calendar)
+    .component('Card', Card)
     .mount('#app')
--- src/services/api.js
+++ src/services/api.js
@@ -1,13 +1,35 @@
 import axios from 'axios'
+import { useCookies } from '@vueuse/integrations/useCookies';
+
+const cookies = useCookies(['logged-in', 'user-type', 'auth-token']);
 
 const api = axios.create({
     baseURL: 'https://localhost:7229',
     timeout: 2000,
+    headers: {
+        'Authorization': useCookies().get('auth-token')
+    }
 })
 
-export const login = (email, password) => {
-    return api.post('/doctor/login', {
+const setTokenAndCookies = (userType, token) => {
+    const cookieOptions = { maxAge: 864000, sameSite: true};
+    api.defaults.headers.common['Authorization'] = token;
+    cookies.set('logged-in', 'true', cookieOptions);
+    cookies.set('user-type', userType, cookieOptions);
+    cookies.set('auth-token', token, cookieOptions);
+}
+
+export const loginDoctor = (email, password) => {
+    api.post('/doctor/login', {
         email: email,
         password: password
+    }).then(response => {
+        setTokenAndCookies('doctor', response.data.token);
+    })
+}
+
+export const createVaccinationSlot = (date) => {
+    return api.post('/doctor/vaccination-slots', {
+        date: date
     });
 }
