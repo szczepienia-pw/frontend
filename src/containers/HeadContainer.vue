f625c484fe882a290839eeed5d0cec3951a1a714
Magda <magdalenafalkowska00@wp.pl>
2022-03-22 19:21:16 +0100

    Added Logout function

--- src/containers/HeadContainer.vue
+++ src/containers/HeadContainer.vue
@@ -1,23 +1,31 @@
 <template>
-    <nav v-if="loggedIn">
-        <div v-if="userType === 'doctor'">
-            
-        </div>
-        <div v-else>
-            
-        </div>
-    </nav>
-    <Toast /> 
+  <nav v-if="!onLoginPage">
+    <Button label="Log out" @click="userLogout" />
+    <div v-if="userType === 'doctor'">doctor</div>
+    <div v-else>patient</div>
+  </nav>
+  <Toast />
 </template>
 
 <script setup>
 import Toast from "primevue/toast";
-import Cookies from 'js-cookie'
+import Button from "primevue/button";
 
-const userType = Cookies.get('user-type')
-const loggedIn = Cookies.get('logged-in')
+import Cookies from "js-cookie";
+import { logout } from "../services/api";
+import { useRouter, useRoute } from "vue-router";
+import { computed } from "vue";
+
+const route = useRoute();
+
+const onLoginPage = computed(() => route.name == 'login');
+const userType = computed(() => onLoginPage.value ? null : Cookies.get('user-type'));
+const router = useRouter();
+
+const userLogout = function () {
+  logout();
+  router.push({ name: "login" });
+};
 </script>
 
-<style lang="scss">
-    
-</style>
+<style lang="scss"></style>
