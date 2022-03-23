968fe231c5cf1ee7f7b5fdde95b41a3eb6dae8b8
Magda <magdalenafalkowska00@wp.pl>
2022-03-23 15:16:32 +0100

    [sp-107] Log out function

--- src/containers/HeadContainer.vue
+++ src/containers/HeadContainer.vue
@@ -1,5 +1,5 @@
 <template>
-  <nav v-if="!onLoginPage">
+  <nav v-if="!!userType">
     <Button label="Log out" @click="userLogout" />
     <div v-if="userType === 'doctor'">doctor</div>
     <div v-else>patient</div>
@@ -13,18 +13,21 @@ import Button from "primevue/button";
 
 import Cookies from "js-cookie";
 import { logout } from "../services/api";
-import { useRouter, useRoute } from "vue-router";
-import { computed } from "vue";
+import { ref, watch } from "vue";
+import { useRouter, useRoute } from 'vue-router'
 
-const route = useRoute();
-
-const onLoginPage = computed(() => route.name == 'login');
-const userType = computed(() => onLoginPage.value ? null : Cookies.get('user-type'));
 const router = useRouter();
+const route = useRoute();
+const userType = ref(Cookies.get('user-type'));
+
+watch(() => route.name, () => {
+    if(!userType.value && route.name != 'login') userType.value = Cookies.get('user-type');
+})
 
 const userLogout = function () {
-  logout();
-  router.push({ name: "login" });
+    userType.value = '';
+    logout();
+    router.push({ name: "login" });
 };
 </script>
 
