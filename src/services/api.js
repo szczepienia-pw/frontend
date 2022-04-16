8d3a976c136b12833e539660775fe812ca1cf5f6
Dawid Alimowski <dawid.alimowski@gmail.com>
2022-04-16 14:34:14 +0200

    Patient visit page details

--- src/services/api.js
+++ src/services/api.js
@@ -97,7 +97,133 @@ export const changeData = (data) => {
 };
 
 export const getVaccinationHistory = (page = 1) => {
-    return api.get(`/patient/vaccinations?page=${page}`)
+    return new Promise((resolve) => {
+        setTimeout(function() {
+          resolve({ data: {
+              pagination: {
+                currentPage: page,
+                totalPages: page+1,
+                currentRecords: 3,
+                totalRecords: 6,
+              },
+              data: [
+                {
+                    id: 1,
+                    vaccine: {
+                        id: 1,
+                        name: "Phizer",
+                        disease: "COVID-19",
+                        requiredDoses: 2,
+                        serialNumber: "000-1311231",
+                    },
+                    vaccinationSlot: {
+                        id: 1,
+                        date: "2019-08-24T14:15:22Z",
+                    },
+                    status: "Canceled",
+                    patient: {
+                        id: 1,
+                        firstName: "Patient",
+                        lastName: "Patient",
+                        pesel: "003005039981",
+                        email: "john.doe@patient.com",
+                        address: {
+                            id: 1,
+                            city: "Biłgoraj",
+                            zipCode: "23-400",
+                            street: "Jana Pawła II",
+                            houseNumber: "20",
+                            localNumber: "1A",
+                        },
+                    },
+                    doctor: {
+                        id: 1,
+                        firstName: "Dr John",
+                        lastName: "Doe",
+                        email: "john.doe@doctor.com",
+                    },
+                },
+                {
+                    id: 2,
+                    vaccine: {
+                        id: 2,
+                        name: "Phizer2",
+                        disease: "COVID-19",
+                        requiredDoses: 2,
+                        serialNumber: "000-1311231",
+                    },
+                    vaccinationSlot: {
+                        id: 2,
+                        date: "2019-08-25T14:15:22Z",
+                    },
+                    status: "Completed",
+                    patient: {
+                        id: 1,
+                        firstName: "Patient",
+                        lastName: "Patient",
+                        pesel: "003005039981",
+                        email: "john.doe@patient.com",
+                        address: {
+                            id: 1,
+                            city: "Biłgoraj",
+                            zipCode: "23-400",
+                            street: "Jana Pawła II",
+                            houseNumber: "20",
+                            localNumber: "1A",
+                        },
+                    },
+                    doctor: {
+                        id: 2,
+                        firstName: "Dr John2",
+                        lastName: "Doe",
+                        email: "john.doe@doctor.com",
+                    },
+                },
+                {
+                    id: 3,
+                    vaccine: {
+                        id: 3,
+                        name: "Phizer3",
+                        disease: "COVID-19",
+                        requiredDoses: 2,
+                        serialNumber: "000-1311231",
+                    },
+                    vaccinationSlot: {
+                        id: 3,
+                        date: "2019-08-26T14:15:22Z",
+                    },
+                    status: "Planned",
+                    patient: {
+                        id: 1,
+                        firstName: "Patient",
+                        lastName: "Patient",
+                        pesel: "003005039981",
+                        email: "john.doe@patient.com",
+                        address: {
+                            id: 1,
+                            city: "Biłgoraj",
+                            zipCode: "23-400",
+                            street: "Jana Pawła II",
+                            houseNumber: "20",
+                            localNumber: "1A",
+                        },
+                    },
+                    doctor: {
+                        id: 3,
+                        firstName: "Dr John3",
+                        lastName: "Doe",
+                        email: "john.doe@doctor.com",
+                    },
+                }
+              ]
+          }})
+        }, 1000);
+      });
+    //return api.get(`/patient/vaccinations?page=${page}`)
+}
+
+export const cancelVaccinationSlot = (id) => {
+    return api.delete(`/patient/vaccination-slots/${id}`);
 }
 
 export const getAdminSettings = () => {
