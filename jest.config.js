09e14128a4260a021ab2ca6f4d7344b46a206aa6
Dawid Alimowski <dawid.alimowski@gmail.com>
2022-05-20 23:46:57 +0200

    Add test code coverage (#65)

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,11 @@
 module.exports = {
-  preset: '@vue/cli-plugin-unit-jest'
+  preset: '@vue/cli-plugin-unit-jest',
+  collectCoverage: true,
+  collectCoverageFrom: ["**/*.{js,vue}", "!**/*.config.js", "!**/node_modules/**", "!**/dist/**", "!**/coverage/**", '!**/main.js'],
+  transform: {
+    "^.+\\.js$": "babel-jest",
+    ".*\\.(vue)$": "@vue/vue3-jest"
+  },
+  coverageProvider: "v8",
+  moduleFileExtensions: ["js", "vue"],
 }
