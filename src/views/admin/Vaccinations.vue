52c7da1444505a6d6cd42467a9d4393e1eb9bb69
Dawid Alimowski <dawid.alimowski@gmail.com>
2022-05-20 14:43:38 +0200

    Remove unused code

--- src/views/admin/Vaccinations.vue
+++ src/views/admin/Vaccinations.vue
@@ -5,18 +5,8 @@
 </template>
 
 <script setup>
-// import Button from "primevue/button";
-// import Calendar from "primevue/calendar";
-// import Card from "primevue/card";
+
 import AdminVaccinationList from '@/components/AdminVaccinationList'
-
-// import { ref } from "vue";
-// import { useToast } from "primevue/usetoast";
-// import { createVaccinationSlot } from "@/services/api";
-// import { errorToast, successToast } from "@/services/helpers";
-
-//const toast = useToast();
-
 
 </script>
 
