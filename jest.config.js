6a48ee1bfb49c32ed8921cad858c42b8e73ab9af
Przemysław Dominikowski <przemyslaw.dominikowski@intel.com>
2022-05-23 08:54:40 +0200

    Revert "Add test code coverage (#65)"
    This reverts commit 09e14128a4260a021ab2ca6f4d7344b46a206aa6.

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,3 @@
 module.exports = {
-  preset: '@vue/cli-plugin-unit-jest',
-  collectCoverage: true,
-  collectCoverageFrom: ["**/*.{js,vue}", "!**/*.config.js", "!**/node_modules/**", "!**/dist/**", "!**/coverage/**", '!**/main.js'],
-  transform: {
-    "^.+\\.js$": "babel-jest",
-    ".*\\.(vue)$": "@vue/vue3-jest"
-  },
-  coverageProvider: "v8",
-  moduleFileExtensions: ["js", "vue"],
+  preset: '@vue/cli-plugin-unit-jest'
 }
